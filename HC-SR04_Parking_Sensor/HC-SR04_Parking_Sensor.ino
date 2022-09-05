4bbeb3fbb1fe9912bfd4e13863d8ac891e1b5d39
Sandro Tributi <sandro.tributi@gmail.com>
2022-09-05 10:25:32 +0200

    Update HC-SR04_Parking_Sensor.ino

--- HC-SR04_Parking_Sensor/HC-SR04_Parking_Sensor.ino
+++ HC-SR04_Parking_Sensor/HC-SR04_Parking_Sensor.ino
@@ -1,3 +1,25 @@
+ /**
+ * Reference: Paolo Aliverti, Progettare con Arduino (Italian Edition)
+ *
+ * Ultrasonic Parking Sensor
+ * 
+ * We must constantly measure the distance from a possible obstacle and emit a sound that is all the 
+ * more rapid and insistent the lower the measurement is. 
+ * We will in fact consider three cases:
+ *  1. obstacle out of range: we will not make any sound;
+ *  2. obstacle too close: we will make a continuous sound;
+ *  3. obstacle at intermediate distance: the sounds are emitted with a frequency inversely proportional
+ *     to the distance (closer, faster and higher).
+ * 
+ * Distances are expressed in centimeters.
+ *
+ * This code is in the public domain. 
+ * 
+ * by Sandro Tributi <sandro.tributi@gmail.com>
+ *
+ * https://github.com/sandrotributi/Arduino-Lab/edit/main/HC-SR04_Parking_Sensor/HC-SR04_Parking_Sensor.ino
+ */
+
 #define trigPin 6
 #define echoPin 7
 #define ledPin 11
